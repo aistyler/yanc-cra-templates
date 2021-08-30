6bc87a7328e9191e3e1b05363f77b7bc0780a15e
peterk <tiglek+u16office@gmail.com>
2021-08-30 22:47:25 +0900

    update styles

--- packages/cra-template-typescript/template/src/components/Layout.tsx
+++ packages/cra-template-typescript/template/src/components/Layout.tsx
@@ -3,13 +3,15 @@ import s from './layout.module.css';
 
 const Layout: React.FC<any> = ({ children }) => {
   return (
-    <div className={s.Layout}>
-      <header className={s.LayoutHeader}>
+    <div>
+      <header className={`${s.LayoutHeader} container`}>
+        <h1>Header</h1>
       </header>
-      <main className={s.LayoutMain}>
+      <main className={`${s.LayoutMain} container`}>
         {children}
       </main>
-      <footer className={s.LayoutFooter}>
+      <footer className={`${s.LayoutFooter} container`}>
+        <h3>Footer</h3>
       </footer>
     </div>
   );
--- packages/cra-template-typescript/template/src/pages/index.tsx
+++ packages/cra-template-typescript/template/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Layout from "../components/Layout";
+import { Layout } from "../components/Layout";
 
 import logo from './logo.svg';
 import './index.css';
@@ -8,20 +8,22 @@ import './index.css';
 const Index: React.FC<any> = () => {
   return (
     <Layout>
-      <header className="page-header">
-        <img src={logo} className="page-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="page-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page-container container">
+        <header className="page-header">
+          <img src={logo} className="page-logo" alt="logo" />
+          <p>
+            Edit <code>src/pages/index.tsx</code> and save to reload.
+          </p>
+          <a
+            className="page-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </Layout>
   );
 }
